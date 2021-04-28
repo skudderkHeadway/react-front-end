3702d2d1f809689681e677a2b6c6ee2b7047f717
skudderkHeadway <83316418+skudderkHeadway@users.noreply.github.com>
2021-04-28 18:12:13 -0400

    Add React Hook Form, test

--- src/components/UrlShortenForm.jsx
+++ src/components/UrlShortenForm.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import Button from "./Button";
 
 const UrlShortenForm = () => {
+  const { register, watch } = useForm({ mode: "all" });
+  console.log(watch());
+
   return (
     <form>
       <input
-        name="url"
+        className="text-black"
         type="text"
         placeholder="Make your links shorter"
         required
+        {...register("url", { required: true })}
       />
       <input name="slug" type="text" placeholder="Customize your link" />
-      <Button type="submit">Shorten URL</Button>
+      <Button type="submit" theme="primary">
+        Shorten URL
+      </Button>
     </form>
   );
 };
